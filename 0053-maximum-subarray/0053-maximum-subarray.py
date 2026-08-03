d17a8c35955c81f893849acccc7fb8b0ec182c4c
Prince Kumar Gupta <princegupta0030@gmail.com>
2026-08-03 23:54:35 +0530

    Time: 103 ms (23.21%), Space: 24.4 MB (10.65%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maxSubArray(self,nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        arr=[]
+        arr.append(nums[0])
+        maxSum=arr[0]
+        for i in range(1,len(nums)):
+            arr.append(max(arr[i-1]+nums[i],nums[i]))
+            if arr[i]>maxSum:
+                maxSum=arr[i]
+        return maxSum 
