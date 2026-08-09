fde8976f06087e0655119dd3759796359945c5ca
Prince Kumar Gupta <princegupta0030@gmail.com>
2026-08-09 22:19:59 +0530

    Time: 3 ms (38.86%), Space: 12.7 MB (9.26%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -9,4 +9,6 @@ class Solution(object):
         for j in range(len(t)):
             if i<len(s) and s[i]==t[j]:
                 i+=1
-        return i==len(s)
+            if i==len(s):
+                return True
+        return len(s)==0
